869cd86efa809e920d190e0c8426a1d881b00edf
Lokitodev <gu.does.dev@gmail.com>
2023-02-01 22:03:45 +0000

    v0: Jogos da Semana

---
+++ src/utils/lolesports.ts
@@ -0,0 +1,100 @@
+import axios from "axios";
+
+//export const ITEMS_URL = "https://ddragon.leagueoflegends.com/cdn/11.5.1/img/item/"
+export const ITEMS_URL =
+  "https://ddragon.bangingheads.net/cdn/latest/img/item/";
+export const CHAMPIONS_URL =
+  "https://ddragon.bangingheads.net/cdn/latest/img/champion/";
+
+const API_URL_PERSISTED = "https://esports-api.lolesports.com/persisted/gw";
+const API_URL_LIVE = "https://feed.lolesports.com/livestats/v1";
+const API_KEY = "0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z";
+
+export function getLiveGames() {
+  return axios.get(`${API_URL_PERSISTED}/getLive?hl=pt-BR`, {
+    headers: {
+      "x-api-key": API_KEY,
+    },
+  });
+}
+
+export function getSchedule(page?: string, leagueIds?: string) {
+  return axios.get(`${API_URL_PERSISTED}/getSchedule?hl=pt-BR`, {
+    params: { pageId: page, leagueId: leagueIds },
+
+    headers: {
+      "x-api-key": API_KEY,
+    },
+  });
+}
+export async function getLeagues(targetLeagues: Array<string>) {
+  return axios
+    .get(`${API_URL_PERSISTED}/getLeagues?hl=pt-BR`, {
+      headers: {
+        "x-api-key": API_KEY,
+      },
+    }) 
+    .then((response: { data: { data: { leagues: [{ slug: string, id: string}] } } }) => {
+      const leagues = response.data.data.leagues;
+
+      const leagueIds = leagues
+        .filter((league) => {
+          return targetLeagues.includes(league.slug);
+        })
+        .map((league) => {
+          return league.id;
+        })
+        .join();
+
+      return {leagues, leagueIds}
+    });
+}
+
+export function getLiveWindowGame(gameId: string, date: string) {
+  return axios.get(`${API_URL_LIVE}/window/${gameId}`, {
+    params: {
+      hl: "pt-BR",
+      startingTime: date,
+    },
+    headers: {
+      "x-api-key": API_KEY,
+    },
+  });
+}
+
+export function getLiveDetailsGame(gameId: string, date: string) {
+  return axios.get(`${API_URL_LIVE}/details/${gameId}`, {
+    params: {
+      hl: "pt-BR",
+      startingTime: date,
+    },
+    headers: {
+      "x-api-key": API_KEY,
+    },
+  });
+}
+
+export function getGameDetails(gameId: string) {
+  return axios.get(`${API_URL_PERSISTED}/getEventDetails`, {
+    params: {
+      hl: "pt-BR",
+      id: gameId,
+    },
+    headers: {
+      "x-api-key": API_KEY,
+    },
+  });
+}
+
+export function getISODateMultiplyOf10() {
+  const date = new Date();
+  date.setMilliseconds(0);
+
+  if (date.getSeconds() % 10 !== 0) {
+    date.setSeconds(date.getSeconds() - (date.getSeconds() % 10));
+  }
+
+  date.setSeconds(date.getSeconds() - 60);
+
+  return date.toISOString();
+}
